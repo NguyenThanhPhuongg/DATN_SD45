e5e6f1c587f897986ea82eb463a6bc3aa6eb8aa3
ThanhPhuongg <phuong306204@gmail.com>
2024-10-10 23:32:34 +0700

    template project

---
+++ src/main/java/org/example/datn/controller/CartController.java
@@ -0,0 +1,25 @@
+package org.example.datn.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping
+public class CartController {
+
+    @GetMapping("/")
+    public String login() {
+        return "customer/login/index";
+    }
+
+    @GetMapping("/cart")
+    public String cart() {
+        return "customer/cart/index";
+    }
+
+
+
+}
---
+++ src/main/resources/static/js/main.js
@@ -0,0 +1,44 @@
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const overLay = document.createElement('div');
+    overLay.classList.add('over_lay');
+    document.body.appendChild(overLay);
+    const navBtnMb = document.querySelector('.nav_btn_mb');
+    const btnSearch = document.querySelector('.icon_search');
+    const headerSearch = document.querySelector('.header_search');
+    const btnCart = document.querySelector('.icon_card');
+    const overLayCart = document.querySelector('.over_lay_cart');
+    const closeCart = document.querySelector('.close-cart');
+
+    if (navBtnMb) {
+        navBtnMb.addEventListener('click', function() {
+            document.body.classList.toggle('open');
+        });
+        overLay.addEventListener('click', function() {
+            document.body.classList.remove('open');
+        });
+    }
+    if (btnSearch && headerSearch) {
+        btnSearch.addEventListener('click', function () {
+            headerSearch.classList.toggle('open_search');
+        });
+
+        document.addEventListener('click', function (event) {
+            if (!headerSearch.contains(event.target) && !btnSearch.contains(event.target)) {
+                headerSearch.classList.remove('open_search');
+            }
+        });
+    }
+    if (btnCart) {
+        btnCart.addEventListener('click', function() {
+            document.body.classList.toggle('open-cart');
+        });
+        overLayCart.addEventListener('click', function() {
+            document.body.classList.remove('open-cart');
+        });
+        closeCart.addEventListener('click', function() {
+            document.body.classList.remove('open-cart');
+        });
+    }
+});
