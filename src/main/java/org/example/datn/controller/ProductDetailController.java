b0c8101964db9ecb91480fa40d37c78ca781ea86
ThanhPhuongg <phuong306204@gmail.com>
2024-10-12 20:52:07 +0700

    done-productDetail-fe

--- src/main/java/org/example/datn/controller/ProductDetailController.java
+++ src/main/java/org/example/datn/controller/ProductDetailController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ProductDetailController {
-    @GetMapping("/productsDetail")
+    @GetMapping("/productDetail")
     public String productDetail() {
         return "customer/productDetail/index";
     }
