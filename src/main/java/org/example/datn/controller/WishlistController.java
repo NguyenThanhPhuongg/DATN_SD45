f69d2d4a007d3bcb294274b10e09534f7eadb7fd
ThanhPhuongg <phuong306204@gmail.com>
2024-10-12 12:23:02 +0700

    wishlist-fe

--- src/main/java/org/example/datn/controller/WishlistController.java
+++ src/main/java/org/example/datn/controller/WishlistController.java
@@ -1,16 +1,15 @@
 package org.example.datn.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("")
+@RequiredArgsConstructor
 public class WishlistController {
 
-
     @GetMapping("/wishlist")
-    public String index() {
-        return "/customer/pages/wishlist";
+    public String wishlist() {
+        return "customer/wishlist/index";
     }
 }
