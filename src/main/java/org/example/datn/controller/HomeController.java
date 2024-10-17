3dbe1957506c6622a94995982c7e0547bc7f0636
Thanh Hieu Pham <ahieu5377@gmail.com>
2024-10-17 11:44:01 +0700

    fe category

--- src/main/java/org/example/datn/controller/HomeController.java
+++ src/main/java/org/example/datn/controller/HomeController.java
@@ -7,7 +7,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 public class HomeController {
     @GetMapping("/")
-    public String home(){
+    public String home() {
         return "customer/home/index";
+    }
+
+    @GetMapping("/category")
+    public String category() {
+        return "customer/category/category";
     }
 }
