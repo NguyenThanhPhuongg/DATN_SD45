e8bce59c2f4415e746e566fb39559f460e67d833
ThanhPhuongg <phuong306204@gmail.com>
2024-11-03 21:46:22 +0700

    quet qr sp

--- src/main/resources/static/staff/js/banhang-ctrl.js
+++ src/main/resources/static/staff/js/banhang-ctrl.js
@@ -1,3 +1,18 @@
-app.controller("banhang-ctrl", function ($scope, $http, $rootScope, $location) {
 
+// Khởi tạo controller AngularJS
+app.controller("banhang-ctrl", function ($scope, $http, $rootScope, $location) {
+    $scope.bills = [];
+    $scope.activeIndex = 0;
+    $scope.scanResults = JSON.parse(localStorage.getItem('qrResults'));
+    // Hàm thêm hóa đơn
+    $scope.addBill = function () {
+        const bill = {
+            price: null,
+            name_cutomsmer: null,
+            phone: null,
+        }
+        $scope.bills.push(bill);
+        $scope.activeIndex = $scope.bills.length - 1; // Cập nhật activeIndex
+
+    };
 });
