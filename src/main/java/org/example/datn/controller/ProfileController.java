4eb6ec3171a9f0da9ca5efb9d95454ef8da3187a
ThanhPhuongg <phuong306204@gmail.com>
2024-11-09 12:23:25 +0700

    KH

--- src/main/java/org/example/datn/controller/ProfileController.java
+++ src/main/java/org/example/datn/controller/ProfileController.java
@@ -9,10 +9,7 @@ import org.example.datn.model.request.ProfileRequest;
 import org.example.datn.processor.ProfileProcessor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
